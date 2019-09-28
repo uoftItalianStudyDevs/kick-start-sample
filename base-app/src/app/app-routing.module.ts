1fb310e234c2cc9b24857e703b457741ec545639
michaelmaozi <michaelmao.zi@gmail.com>
2019-09-28 00:28:54 -0400

    enable router for template and new-template pages

--- base-app/src/app/app-routing.module.ts
+++ base-app/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { TemplateNameComponent } from './components/template-name/template-name.component';
+import { NewTempalteNameComponent } from './components/template-name/new-tempalte-name/new-tempalte-name.component';
 
 
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
-  // {path: 'templateName', component: templateNam},
-  // {path: 'newTemplateName', component: NewTemplateComponent}
+  {path: 'templateName', component: TemplateNameComponent},
+  {path: 'newTemplateName', component: NewTempalteNameComponent}
 ];
 
 @NgModule({
